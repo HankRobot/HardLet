ca80ca7d50a2faa3a47ca6439e28e5c3a788e782
HankRobot <thc1n17@soton.ac.uk>
2019-07-12 16:55:36 +0800

    nice

--- HardLet/HardLet/MainWindow.xaml.cs
+++ HardLet/HardLet/MainWindow.xaml.cs
@@ -22,73 +22,184 @@ namespace HardLet
     /// </summary>
     public partial class MainWindow : Window
     {
-        private byte btnstate = 0;
+        /// <summary>
+        /// For Serial ports
+        /// </summary>
         private bool connected = false;
+        string comdata;
         private static SerialPort mySerialPort;
+
+        /// <summary>
+        /// For 6 pin button
+        /// </summary>
+        private int value = 1;
+        private bool oneset = false;
+        private bool twoset = false;
+        private bool threeset = false;
+        private bool fourset = false;
+        private bool fiveset = false;
+        private bool sixset = false;
+        public IDictionary<string,int> buttonseq = new Dictionary<string, int>()
+        {
+            {"1",0},
+            {"2",0},
+            {"3",0},
+            {"4",0},
+            {"5",0},
+            {"6",0},
+        };
 
         public MainWindow()
         {
             InitializeComponent();
-            string[] ports = SerialPort.GetPortNames();
-            foreach (string port in ports)
-            {
-                Console.WriteLine(port);
-            }
             COMList.ItemsSource = SerialPort.GetPortNames();
         }
 
         private void Connection_Click(object sender, RoutedEventArgs e)
         {
-            if (connected)
+            if (COMList.SelectedItem != null)
             {
-                connected = false;
-                mySerialPort.Close();
+                if (connected)
+                {
+                    connected = false;
+                    Connection.Content = "Connect";
+                    mySerialPort.Close();
+                    MessageBox.Show("COM port disconnected", "COM port Status");
+                }
+                else
+                {
+                    connected = true;
+                    try
+                    {
+                        Connection.Content = "Disconnect";
+                        for (int i = 1; i < buttonseq.Count+1; i++)
+                        {
+                            Console.WriteLine(buttonseq[i.ToString()]);
+                        }
+                        mySerialPort = new SerialPort(COMList.SelectedItem.ToString(), 115200);
+                        mySerialPort.Open();
+                        MessageBox.Show("COM port connected", "COM port Status");
+                        mySerialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataRead);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "COM port not selected");
+                    }
+                }
             }
             else
             {
-                connected = true;
-                mySerialPort = new SerialPort(COMList.SelectedItem.ToString(), 115200);
-                try
-                {
-                    mySerialPort.Open();
-                    mySerialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataRead);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.ToString(), "COM port not selected");
-                }
+                MessageBox.Show("COM port not selected", "COM port status");
             }
         }
 
         void SerialDataRead(object sender, SerialDataReceivedEventArgs e)
         {
-            String comdata = mySerialPort.ReadLine();
-            Dispatcher.Invoke((Action)(() => Console.WriteLine("Data\n" + comdata + "\n")));
+            comdata = mySerialPort.ReadLine();
+            Dispatcher.Invoke((Action)(() => Console.Write("Data: " + comdata + "\n")));
         }
 
         private void One_Click(object sender, RoutedEventArgs e)
         {
-
+            if (oneset)
+            {
+                oneset = false;
+                one.Content = "";
+                buttonseq["1"] = 0;
+                value--;
+            }
+            else
+            {
+                oneset = true;
+                one.Content = value.ToString();
+                buttonseq["1"] = value;
+                value++;
+            }
         }
-
         private void Two_Click(object sender, RoutedEventArgs e)
         {
-
+            if (twoset)
+            {
+                twoset = false;
+                two.Content = "";
+                buttonseq["2"] = 0;
+                value--;
+            }
+            else
+            {
+                twoset = true;
+                two.Content = value.ToString();
+                buttonseq["2"] = value;
+                value++;
+            }
         }
-
         private void Three_Click(object sender, RoutedEventArgs e)
         {
-
+            if (threeset)
+            {
+                threeset = false;
+                three.Content = "";
+                buttonseq["3"] = 0;
+                value--;
+            }
+            else
+            {
+                threeset = true;
+                three.Content = value.ToString();
+                buttonseq["3"] = value;
+                value++;
+            }
         }
-
         private void Four_Click(object sender, RoutedEventArgs e)
         {
-
+            if (fourset)
+            {
+                fourset = false;
+                four.Content = "";
+                buttonseq["4"] = 0;
+                value--;
+            }
+            else
+            {
+                fourset = true;
+                four.Content = value.ToString();
+                buttonseq["4"] = value;
+                value++;
+            }
         }
-
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            if (fiveset)
+            {
+                fiveset = false;
+                five.Content = "";
+                buttonseq["5"] = 0;
+                value--;
+            }
+            else
+            {
+                fiveset = true;
+                five.Content = value.ToString();
+                buttonseq["5"] = value;
+                value++;
+            }
+        }
         private void Six_Click(object sender, RoutedEventArgs e)
         {
-
+            if (sixset)
+            {
+                sixset = false;
+                six.Content = "";
+                buttonseq["6"] = 0;
+                value--;
+            }
+            else
+            {
+                sixset = true;
+                six.Content = value.ToString();
+                buttonseq["6"] = value;
+                value++;
+            }
         }
     }
 }
